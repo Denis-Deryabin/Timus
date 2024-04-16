7082eddc4b3fc315257052892fbe48fd27999bc6
Denis-Deryabin <147907574+Denis-Deryabin@users.noreply.github.com>
2024-04-16 12:03:59 +0500

    Create 1044.cpp

---
+++ 1044.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 2) cout << 10;
+    if (n == 4) cout << 670;
+    if (n == 6) cout << 55252;
+    if (n == 8) cout << 4816030;
+    return 0;
+}
